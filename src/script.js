8c33862f9a76f298bb1bdbe1dc28fdfd045f9878
VIJAY KUMAR <vizi.d31@gmail.com>
2023-03-10 17:36:29 +0530

    done front end  using react

---
+++ src/script.js
@@ -0,0 +1,13 @@
+// Dark & Light toggle
+
+document.querySelector(".day-night input").addEventListener("change", () => {
+  document.querySelector("body").classList.add("toggle");
+  setTimeout(() => {
+    document.querySelector("body").classList.toggle("light");
+
+    setTimeout(
+      () => document.querySelector("body").classList.remove("toggle"),
+      10
+    );
+  }, 5);
+});
